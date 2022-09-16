93a2727f7e4f4e623c61cbfee892b3568eb03bf8
Imanprokeren <98250257+Imanprokeren@users.noreply.github.com>
2022-09-16 21:21:03 +0700

    Update i-donasi.js

--- plugins/i-donasi.js
+++ plugins/i-donasi.js
@@ -8,13 +8,13 @@ let handler = async (m, { conn, usedPrefix }) => {
       extendedTextMessage: {
       text: `
 ╭─「 Donasi • Dana 」
-│ • Triii [62895604670507]
-│ • Gopay  [62895604670507]
-│ • Dana  [62895604670507]
+│ • Triii [62895385157376]
+│ • Gopay  [62895385157376]
+│ • Dana  [62895385157376]
 │ • Qris  [Chat OWNER]
 ╰────
 ╭─「 *NOTE* 」
-│ > Ingin donasi? Wa.me/62895604670507
+│ > Ingin donasi? Wa.me/6289522724388
 │ _Hasil donasi akan digunakan buat sewa_
 │ _atau beli *RDP/VPS* agar bot bisa jalan_
 │ _24jam tanpa kendala_
