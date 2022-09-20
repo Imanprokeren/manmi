eb26c32efd7e041bf243570b405d09ef69f4b56d
Imanprokeren <98250257+Imanprokeren@users.noreply.github.com>
2022-09-20 07:43:55 +0700

    Add files via upload

--- plugins/menfess.js
+++ plugins/menfess.js
@@ -1,65 +1,33 @@
-const { MessageType } = require('@adiwajshing/baileys')
-let fetch = require('node-fetch')
-let handler = async(m, { conn, text }) => {
+let handler = async(m, { conn, text, usedPrefix }) => {
 let [number, pesan] = text.split `|`
 
-    if (!number) return conn.reply(m.chat, 'Penggunaan .chat nomor|pesan untuknya\nContoh : .chat 628xxxxxxxxxx|hai owner', m, {quoted: m, thumbnail: await (await fetch('https://telegra.ph/file/b31b5f0109ff5bd098704.jpg')).buffer(),
-        contextInfo: { forwardingScore: 999, isForwarded: true, externalAdReply: {title: 'Gunakan Dengan Bijak Yah Kak', sourceUrl: 'http://wa.me/62895604670507?text=.chat 62895604670507|Hi Zens Hehehe 🌹', body: 'Yor  Botz By Zens', thumbnail: await (await fetch('https://telegra.ph/file/b31b5f0109ff5bd098704.jpg')).buffer(),}}})
-    if (!pesan) return conn.reply(m.chat, 'Silahkan masukan pesannya', m, {quoted: m, thumbnail: await (await fetch('https://telegra.ph/file/b9a32ee41970d7a71b476.jpg')).buffer(),
-        contextInfo: { forwardingScore: 999, isForwarded: true, externalAdReply: {title: 'Gunakan Dengan Bijak Yah Kak', sourceUrl: 'http://wa.me/62895604670507?text=.chat 62895604670507|Hi Zens Hehehe 🌹', body: 'Yor  Botz By Zens', thumbnail: await (await fetch('https://telegra.ph/file/b31b5f0109ff5bd098704.jpg')).buffer(),}}})
-    if (text > 700) return conn.reply(m.chat, 'Teks Kepanjangan!', m, {quoted: m, thumbnail: await (await fetch('https://telegra.ph/file/b9a32ee41970d7a71b476.jpg')).buffer(),
-        contextInfo: { forwardingScore: 999, isForwarded: true, externalAdReply: {title: 'Gunakan Dengan Bijak Yah Kak', sourceUrl: 'http://wa.me/62895604670507?text=.chat 62895604670507|Hi Zens Hehehe 🌹', body: 'Yor  Botz By Zens', thumbnail: await (await fetch('https://telegra.ph/file/b31b5f0109ff5bd098704.jpg')).buffer(),}}})
+    if (!number) return conn.reply(m.chat, 'Silahkan masukan nomor dan pesan yang akan dikirim\n\n.menfes 628xxxx|isi pesan', m)
+    if (!pesan) return conn.reply(m.chat, 'Silahkan masukan pesannya\n.menfes 628xxxx|isi pesan', m)
+    if (text > 500) return conn.reply(m.chat, 'Teks Kepanjangan!', m)
+    
+    let user = global.db.data.users[m.sender]
 
-    let chat = `${number}`
+    let korban = `${number}`
     var nomor = m.sender
-    let chat1 = `Hi Saya Bot Ada Yang Kirim Pesan Ke Kamu
-Seseorang Temanmu
-(Pengirim Rahasia)
 
-⬡──⬡─────────⬡──⬡
+    conn.reply(korban + '@s.whatsapp.net',  `*「 Pesan Menfes! 」*\n\nDari : _Secret Number!_\nPesan : _${pesan}_\n\n*${global.wm}*`
+, null)
 
-💌 Pesan : ${pesan}
-
-⬡──⬡─────────⬡──⬡
-
-Maaf Anda Belum Bisa Membalas ke Pengirim`
-
-let haori = `▮PENGIRIM RAHASIA 」 
-Anda Ingin Mengirimkan Pesan ke pacar/sahabat/teman/doi/
-mantan?, tapi Tidak ingin tau siapa Pengirimnya?
-Kamu bisa menggunakan Bot ini
-Contoh Penggunaan: .chat nomor|pesan untuknya
-
-Contoh: #chat 628xxxxxxxxxx|hai owner`
-
-    
-    let logs = `BOT AKAN BLOKIR KONTAKMU?
-    
-≫ Spam
-≫ Chat Aneh Aneh
-≫ Berantem`
-
- let haori1 = `Sukses Mengirim Pesan
-👥 Dari : wa.me/${nomor.split("@s.whatsapp.net")[0]}
-
-⬡──⬡─────────⬡──⬡
-
-Isi Pesan : ${pesan}
-
-⬡──⬡─────────⬡──⬡`
-handler.help = ['menfes <nomor|pesan>']
-handler.tags = ['menfes']
-handler.command = /^(chat|menfess)$/i
-handler.owner = false
-handler.mods = false
+    conn.reply(m.chat, `_*Pesan Menfes Berhasil Terkirim Ke ${korban}!*_`, m)
+}
+handler.help = ['menfes nomor|pesan']
+handler.tags = ['new']
+handler.command = /^(menfess|menfes)$/i
+handler.rowner = false
+handler.limit = true
 handler.premium = false
 handler.group = false
-handler.private = true
-
+handler.private = false
+//plugins by Ndaa!!
 handler.admin = false
 handler.botAdmin = false
 
 handler.fail = null
-handler.limit = false
+handler.limit = true
 
 module.exports = handler
